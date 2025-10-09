6bcbad9a6f26f69651caf447cc922d3234e2faea
Kónya Gellért <konyag@KKSZKI.LOCAL>
2025-10-09 09:22:47 +0200

    feladat 1 is ready

---
+++ gyakorlas/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyakorlas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            int ido = DateTime.Now.Hour;
+            if (ido < 12)
+            {
+                Console.WriteLine("Jó reggelt!");
+            }
+            else if (ido < 18)
+            {
+                Console.WriteLine("Jó napot!");
+            }
+            else
+            {
+                Console.WriteLine("Jó estét!");
+            }
+            Console.WriteLine("--------------------------------------------------------------");
+            //2
+
+        }
+    }
+}
