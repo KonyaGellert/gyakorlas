f52170d9226784c700c80666f88c46ea7ee09a19
Kónya Gellért <konyag@KKSZKI.LOCAL>
2025-10-09 09:46:42 +0200

    feladat2 id ready

--- gyakorlas/Program.cs
+++ gyakorlas/Program.cs
@@ -26,6 +26,25 @@ namespace gyakorlas
             }
             Console.WriteLine("--------------------------------------------------------------");
             //2
+            Console.Write("Kérem a lekötés összegét: ");
+            double osszeg = double.Parse(Console.ReadLine());
+
+            Console.Write("Hány évre szeretné lekötni?: ");
+            int ev = int.Parse(Console.ReadLine());
+
+            Console.Write("Milyen kamatra?:(%) ");
+            double kamat = double.Parse(Console.ReadLine());
+            
+            double kamat2 = kamat / 100 + 1;
+
+            for (int i = 0; i < ev; i++)
+            {
+                osszeg *= kamat2;
+            }
+
+            Console.WriteLine($"A lekötés végén az összeg: {osszeg} Ft");
+
+
 
         }
     }
